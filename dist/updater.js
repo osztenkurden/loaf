ba90f22452fc2b2ddaeb7927696c09ab9d4a6ae3
Hubert Walczak <hubertwalczak8@gmail.com>
2022-03-22 11:43:54 +0100

    Added dev options

--- dist/updater.js
+++ dist/updater.js
@@ -1,4 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_updater_1 = require("electron-updater");
-electron_updater_1.autoUpdater.checkForUpdatesAndNotify();
+electron_updater_1.autoUpdater.on("checking-for-update", () => {
+    console.log("checking for update");
+});
+electron_updater_1.autoUpdater.on("update-available", () => {
+    console.log("update available");
+});
+electron_updater_1.autoUpdater.on("update-not-available", () => {
+    console.log("update not available");
+});
+electron_updater_1.autoUpdater.checkForUpdates();
--- updater.ts
+++ updater.ts
@@ -1,3 +1,15 @@
 import { autoUpdater } from 'electron-updater';
 
-autoUpdater.checkForUpdatesAndNotify();
+autoUpdater.on("update-available", () => {
+    console.log("update available");
+});
+
+autoUpdater.on("update-not-available", () => {
+    console.log("update not available");
+});
+
+autoUpdater.on("download-progress", () => {
+    console.log("Downloading...");
+});
+
+autoUpdater.checkForUpdates();
